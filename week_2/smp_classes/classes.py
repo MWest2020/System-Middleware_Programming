03c69d3cd732f767419b9655426fde639b4c2d91
Mark Westerweel <mark.westerweel@hva.nl>
2023-04-23 23:32:15 +0200

    week_2 classes done

--- week_2/smp_classes/classes.py
+++ week_2/smp_classes/classes.py
@@ -27,6 +27,11 @@ class Student:
 Pieter = Student("Pieter", [6.0, 7.0, 8.0])
 Jimmy = Student("Jimmy", [5.0, 4.0, 3.0])
 Jack = Student("Jack", [5.0, 7.0, 8.0])
+Timmie = Student("Timmie", [5.0, 10.0, 8.0])
+Sandy = Student("Sandy", [9.0, 7.0, 8.0])
+Jaqueline = Student("Jaqueline", [5.0, 4.0, 3.0])
+
+print(Jack.calc_average())
 
 
 class Course:
@@ -37,21 +42,22 @@ class Course:
     def add_student(self, Student):
         self.students.append(Student)
 
-    def calc_average(self, students):
-        average = sum(Student.grades) / len(students)
-        return average
+    def calc_average(self):
+        return [student.calc_average() for student in self.students]
 
     def check_studentBSA(self):
         negative_BSA = []
         for student in self.students:
             if not student.achieve_BSA():
                 negative_BSA.append(str(student))
-        print("The folowing students did not achieve the BSA: ", (negative_BSA))
+        # breaking up print statement to make it pep8 compliant
+        print("The following students did not achieve the BSA: ", +
+              (negative_BSA))
 
 
-Biology = Course("Biology", [Pieter, Jimmy, Jack])
+Biology = Course("Biology", [Pieter, Jimmy, Jack, Timmie, Sandy, Jaqueline])
 
-Biology.check_studentBSA()
+# Biology.check_studentBSA()
 
 
 class Faculty:
@@ -66,14 +72,26 @@ class Faculty:
         average = sum(self.courses) / len(self.courses)
         return average
 
-    def get_excellence(self, Course):
-        excellence = []
+    def get_best_3_grades(self, grades):
+        sorted_grades = sorted(grades, reverse=True)
+        return sorted_grades[:3]
+
+    def get_excellence(self):
         for course in self.courses:
-            for student in course.students:
-                print("Idaho")
+            print(f"Top 3 students in {course.name}:")
+            top_students = sorted(
+                course.students,
+                # key=lamba needed to sort by value, not by object
+                key=lambda x: x.calc_average(),
+                reverse=True)[
+                :3]
+            for i, student in enumerate(top_students):
+                print(f"{i+1}. {student.name} ({student.calc_average():.1f})")
+            print()
+
 
 faculty = Faculty("Science", [Biology])
 
 
-
-faculty.get_excellence(Biology.students)
+faculty.get_excellence()
+faculty.print_top_3_students()

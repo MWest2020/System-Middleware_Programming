f48a70afe1f6c4aefda94a66713b2b95dfbf0c2b
Mark Westerweel <mark.westerweel@hva.nl>
2023-05-11 15:43:16 +0200

    exercise complete

--- week_2/smp_dicts/make_santa_csv.py
+++ week_2/smp_dicts/make_santa_csv.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import csv
 import random
 from faker import Faker
@@ -12,13 +14,14 @@ def generate_people_csv(num_people, filename):
         birth_date = fake.date_of_birth(minimum_age=1, maximum_age=18)
         postal_code = fake.postcode()
         house_number = random.randint(1, 100)
-        is_good = random.choice([True, False])
-        people.append((name, birth_date, f"{postal_code} {house_number}", is_good))
+        gift_given = random.choice([True, False])
+        behavior = random.choice(["goed", "slecht"])
+        people.append((name, birth_date, f"{postal_code} {house_number}", gift_given, behavior))
 
     # Write the data to a CSV file with the given `filename`
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Naam', 'geboortedata', 'Postcode', 'Stout?'])
+        writer.writerow(['naam', 'geboortedata', 'Postcode', 'gift_given', 'gedrag'])
         for person in people:
             writer.writerow(person)
 
--- week_2/smp_dicts/smp_dicts.py
+++ week_2/smp_dicts/smp_dicts.py
@@ -1,7 +1,12 @@
-import csv
+#!/usr/bin/env python3
 
-# Read all data and store in dictionary (key = name, value = data(?)    )
+import csv
+import random
+
+# Read all data and store in dictionary (key = name, value = data(?))
+# General function for any csv to dict
 def csv_to_dict(filename):
+    # list to store dicts
     result = []
 
     with open('kids_EU.csv', 'r') as f:
@@ -12,11 +17,47 @@ def csv_to_dict(filename):
     
     return result;
 
-print(csv_to_dict('kids_EU.csv'))
+eu = csv_to_dict('kids_EU.csv')
+print(eu)
+
+def gift_randomizer() -> str:
+    dice_roll = random.randint(1, 6)
+
+    if dice_roll == 1:  
+        return ""
+    elif dice_roll == 2:
+        return "pony"
+    elif dice_roll == 3:
+        return "ps5"
+    elif dice_roll == 4:
+        return "nintendo switch"
+    elif dice_roll == 5:
+        return "lego"
+    elif dice_roll == 6:
+        return "barbie"
 
 
 
+# # Assign each kid a present or charcoal
+# # only one parameter, the dictionary, because this function is only for one type of dict. 
+def assign_kid_present(list):
+    key = "gift_given"
+    for dict in list:
+        if dict[key] == "False":
+           dict["cadeau"] = gift_randomizer() 
+           dict[key] == "True"
+           string_goed_of_slecht = "goed" if dict.get('gedrag', 'goed') else "stout"
+           
+           print(f"{dict.get('naam')} was {string_goed_of_slecht}. We hebben een {dict['cadeau']} onder de boom gelegd")             
+        else:
+            print(f"oh, nee, {dict.get('naam')} heeft al een cadeau gekregen")
 
+assign_kid_present(eu)
+
+
+# def main():
+#     csv_to_dict('kids_EU.csv')
+#     assign_kid_present(dict, "gift_given")
 
 # Assign each kid a present or charcoal
 

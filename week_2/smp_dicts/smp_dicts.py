9b4242d88c23740b537ff14d8a33d5f9274a5f34
Mark Westerweel <mark.westerweel@hva.nl>
2023-05-12 08:21:34 +0200

    works natively with the bools

--- week_2/smp_dicts/smp_dicts.py
+++ week_2/smp_dicts/smp_dicts.py
@@ -2,28 +2,41 @@
 
 import csv
 import random
+import ast
 
 # Read all data and store in dictionary (key = name, value = data(?))
 # General function for any csv to dict
+
+
 def csv_to_dict(filename):
     # list to store dicts
     result = []
 
     with open('kids_EU.csv', 'r') as f:
         reader = csv.DictReader(f)
+        
         for row in reader:
-            # 
-            result.append(dict(row))
-    
-    return result;
+            value = row['gift_given']
+            boolean_value = convert_to_bool(value)
+            row["gift_given"] = boolean_value
+            
+            result.append(row)
 
-eu = csv_to_dict('kids_EU.csv')
-print(eu)
+    return result
+
+
+def convert_to_bool(value):
+    try:
+        return ast.literal_eval(value)
+    except (SyntaxError, ValueError):
+        return None
+
+
 
 def gift_randomizer() -> str:
     dice_roll = random.randint(1, 6)
 
-    if dice_roll == 1:  
+    if dice_roll == 1:
         return ""
     elif dice_roll == 2:
         return "pony"
@@ -38,33 +51,36 @@ def gift_randomizer() -> str:
 
 
 
-# # Assign each kid a present or charcoal
-# # only one parameter, the dictionary, because this function is only for one type of dict. 
-def assign_kid_present(list):
-    key = "gift_given"
-    for dict in list:
-        if dict[key] == "False":
-           dict["cadeau"] = gift_randomizer() 
-           dict[key] == "True"
-           string_goed_of_slecht = "goed" if dict.get('gedrag', 'goed') else "stout"
-           
-           print(f"{dict.get('naam')} was {string_goed_of_slecht}. We hebben een {dict['cadeau']} onder de boom gelegd")             
-        else:
-            print(f"oh, nee, {dict.get('naam')} heeft al een cadeau gekregen")
 
-assign_kid_present(eu)
+# Write a function that automatically assigns a gift to a kid
+#  - use complete dictionary as parameter
+#  - Change assign gift from false to true
+#  - print a string: "{Kidname} was {good / bad} has been assigned a gift. {giftname} delivered under tree"
+# - if kid has been assigned a gift, print a string: "{Kidname} has already been assigned a gift. Hopefully {giftname} is desireably"
+
+def assign_kid_present(kids_list):
+    key = "gift_given"
+    for child in kids_list:
+        if not child[key]:
+            child["cadeau"] = gift_randomizer()
+            string_goed_of_slecht = "goed" if child.get('gedrag', 'goed') else "stout"
+            print(f"{child.get('naam')} was {string_goed_of_slecht}. We hebben een {child['cadeau']} onder de boom gelegd")
+            # not setting to True -- csv read only
+            child[key] = True
+        else:
+            print(f"Oh nee, {child.get('naam')} heeft al een cadeau gekregen")
+       
+
+kids_list = csv_to_dict('kids_EU.csv')
+
+
+assign_kid_present(kids_list)
+
 
 
 # def main():
 #     csv_to_dict('kids_EU.csv')
 #     assign_kid_present(dict, "gift_given")
 
-# Assign each kid a present or charcoal
 
-# Track if kid has been given a gift in the dictionary (key gift_given, value a bool)
 
-# Write a function that automatically assigns a gift to a kid
-#  - use complete dictionary as parameter
-#  - Change assign gift from false to true
-#  - print a string: "{Kidname} was {good / bad} has been assigned a gift. {giftname} delivered under tree" 
-# - if kid has been assigned a gift, print a string: "{Kidname} has already been assigned a gift. Hopefully {giftname} is desireably"

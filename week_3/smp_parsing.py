a605d8a6e0eef52adf57804f513f7ee8047b46f0
Mark Westerweel <mark.westerweel@hva.nl>
2023-05-13 22:24:14 +0200

    question2

--- week_3/smp_parsing.py
+++ week_3/smp_parsing.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-
 import csv
-import os 
-
-filename = 'output.csv'
 
 # read from csv and print line by line
 
@@ -15,9 +11,22 @@ def read_csv_and_print(filename):
         for row in reader:
             print(row)
 
-read_csv_and_print('output.csv')
+# read_csv_and_print('output.csv')
 
+# from dict to list. -- why? 
+def from_dict_make_list(filename):
+    result = []
+    
+    with open(filename, 'r') as f:
+        reader = csv.DictReader(f)
+    
+        for row in reader:
+            result.append([value for value in row.values()])
+    
+    return result
 
+# list = from_dict_make_list('output.csv')
+# print(list)
 
 
 
